aad70f897ceb4f76a244378c2c44fbec6b346357
q1w2e3r4god <100752547+q1w2e3r4god@users.noreply.github.com>
2022-09-08 16:25:20 +0900

    Add files via upload

---
+++ AA17_Rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what){
+    console.log("Hello " + what + " !");
+}
+
+hello("AA17");
+hello("redwoods, 이승준");
---
+++ AA17_Rpt02/start/index_AA17.js
@@ -0,0 +1,5 @@
+var myinfo = require('./AA17info');
+
+myinfo("AA17", "Redwoods", '010-1234-5678');
+
+myinfo("AA55", "Comsi", '010-5678-1234');
